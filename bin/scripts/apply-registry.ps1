acdc50f1a4e20aea2604d0576318fb31dcc81281
amitxv <86924251+amitxv@users.noreply.github.com>
2023-06-07 15:43:19 +0100

    Registry import error check

--- bin/scripts/apply-registry.ps1
+++ bin/scripts/apply-registry.ps1
@@ -10,8 +10,13 @@ function Is-Admin() {
 }
 
 function Apply-Registry($file_path) {
-    regedit.exe /s `"$file_path`"
-    C:\bin\NSudo.exe -U:T -P:E -ShowWindowMode:Hide regedit.exe /s `"$file_path`"
+    reg.exe import `"$file_path`" | Out-Null
+    $normal_call = $LASTEXITCODE
+
+    C:\bin\NSudo.exe -U:T -P:E -ShowWindowMode:Hide reg.exe import `"$file_path`" | Out-Null
+    $nsudo_call = $LASTEXITCODE
+
+    return $normal_call -band $nsudo_call
 }
 
 if (!(Is-Admin)) {
@@ -22,18 +27,24 @@ if (!(Is-Admin)) {
 foreach ($file in @("7.reg", "7+.reg", "7-8.reg", "8.reg", "8+.reg", "10.reg", "10+.reg", "11+.reg")) {
     $file_name = $file.replace(".reg", "")
     $file = "C:\bin\registry\$file"
+    $is_successful = 0
 
     if ($file_name.Contains("+")) {
         if ([int]$file_name.replace("+", "") -le $winver) {
-            Apply-Registry($file)
+            $is_successful = Apply-Registry($file)
         }
     } elseif ($file_name.Contains("-")) {
         $lower, $upper = $file_name.Split("-")
         if (($winver -ge $lower) -and ($winver -le $upper)) {
-            Apply-Registry($file)
+            $is_successful = Apply-Registry($file)
         }
     } elseif ([int]$file_name -eq $winver) {
-        Apply-Registry($file)
+        $is_successful = Apply-Registry($file)
+    }
+
+    if ($is_successful -ne 0) {
+        Write-Host "error: failed merging one or more registry files"
+        exit
     }
 }
 
