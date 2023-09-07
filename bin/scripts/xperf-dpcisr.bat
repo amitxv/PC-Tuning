a9ec3bcbbd54ae7bea93f12dc092e190727efae0
AMIT <86924251+amitxv@users.noreply.github.com>
2023-09-07 21:31:13 +0100

    Switch from xperf to wpr for recording traces

--- bin/scripts/xperf-dpcisr.bat
+++ bin/scripts/xperf-dpcisr.bat
@@ -2,25 +2,37 @@
 
 DISM > nul 2>&1 || echo error: administrator privileges required >&2 && exit /b 1
 
-where xperf.exe > nul 2>&1
-if not %errorlevel% == 0 (
-    echo error: xperf not found in path. install "Windows Performance Toolkit" in the ADK from the link below >&2
+set "err=0"
+
+for %%a in ("xperf", "wpr") do (
+    where %%~a > nul 2>&1
+    if not %errorlevel% == 0 (
+        set "err=1"
+    )
+)
+
+if %err% == 1 (
+    echo error: install "Windows Performance Toolkit" from the ADK in the link below >&2
     echo https://learn.microsoft.com/en-us/windows-hardware/get-started/adk-install
     exit /b 1
 )
 
-set "record_delay=3"
-set "record_duration=5"
+set "record_delay=0"
+set "record_duration=1"
+set "trace=C:\trace.etl"
+set "report=C:\report.txt"
 
 echo info: starting in %record_delay%s
-timeout -t %record_delay%
+timeout -t %record_delay% > nul 2>&1
 
 echo info: recording for %record_duration%s
-xperf -on base+interrupt+dpc
-timeout -t %record_duration%
+wpr -start Registry
+timeout -t %record_duration% > nul 2>&1
 
-xperf -stop
-xperf -i "C:\kernel.etl" -o "C:\report.txt" -a dpcisr
-echo info: report saved in C:\
+echo info: saving %trace%
+wpr -stop "%trace%"
+
+echo info: saving %report%
+xperf -quiet -i "%trace%" -o "%report%" -a dpcisr
 
 exit /b 0
