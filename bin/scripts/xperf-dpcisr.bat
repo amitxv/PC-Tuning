3c26a561d8304e018ce7d898a2997d05c324fc5d
AMIT <86924251+amitxv@users.noreply.github.com>
2023-09-18 12:16:16 +0100

    Revert to xperf instead of wpr to prevent large trace files

--- bin/scripts/xperf-dpcisr.bat
+++ bin/scripts/xperf-dpcisr.bat
@@ -2,37 +2,25 @@
 
 DISM > nul 2>&1 || echo error: administrator privileges required >&2 && exit /b 1
 
-set "err=0"
-
-for %%a in ("xperf", "wpr") do (
-    where %%~a > nul 2>&1
-    if not %errorlevel% == 0 (
-        set "err=1"
-    )
-)
-
-if %err% == 1 (
-    echo error: install "Windows Performance Toolkit" from the ADK in the link below >&2
+where xperf.exe > nul 2>&1
+if not %errorlevel% == 0 (
+    echo error: xperf not found in path. install "Windows Performance Toolkit" in the ADK from the link below >&2
     echo https://learn.microsoft.com/en-us/windows-hardware/get-started/adk-install
     exit /b 1
 )
 
 set "record_delay=3"
 set "record_duration=5"
-set "trace=C:\trace.etl"
-set "report=C:\report.txt"
 
 echo info: starting in %record_delay%s
 timeout -t %record_delay% > nul 2>&1
 
 echo info: recording for %record_duration%s
-wpr -start GeneralProfile
+xperf -on base+interrupt+dpc
 timeout -t %record_duration% > nul 2>&1
 
-echo info: saving %trace%
-wpr -stop "%trace%"
-
-echo info: saving %report%
-xperf -quiet -i "%trace%" -o "%report%" -a dpcisr
+xperf -stop > nul 2>&1
+xperf -quiet -i "C:\kernel.etl" -o "C:\report.txt" -a dpcisr
+echo info: report saved in C:\
 
 exit /b 0
