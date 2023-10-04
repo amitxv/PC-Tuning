6db3eab021788051c91dc32a02dcb11edae0d0c0
AMIT <86924251+amitxv@users.noreply.github.com>
2023-10-04 19:06:26 +0100

    Add deprecation note for win-debloat.sh

--- win-debloat.sh
+++ win-debloat.sh
@@ -1,3 +1,5 @@
+# This script will be deprecated and removed in ~November 2023
+
 cd "$(dirname "$0")"
 shopt -s extglob
 
@@ -7,13 +9,13 @@ then
     exit 1
 fi
 
-rm -rf Program\ Files/WindowsApps/*
-rm -rf ProgramData/Packages/!("MicrosoftWindows.Client.CBS_cw5n1h2txyewy")
-rm -rf Users/*/AppData/Local/Microsoft/WindowsApps/!("MicrosoftWindows.Client.CBS_cw5n1h2txyewy")
-rm -rf Users/*/AppData/Local/Packages/!("Microsoft.Windows.ShellExperienceHost_cw5n1h2txyewy"|"windows.immersivecontrolpanel_cw5n1h2txyewy"|"MicrosoftWindows.Client.CBS_cw5n1h2txyewy")
-rm -rf Windows/SystemApps/!("ShellExperienceHost_cw5n1h2txyewy"|"Microsoft.UI.Xaml.CBS_8wekyb3d8bbwe"|"MicrosoftWindows.Client.CBS_cw5n1h2txyewy"|"MicrosoftWindows.Client.Core_cw5n1h2txyewy")
+# rm -rf Program\ Files/WindowsApps/*
+# rm -rf ProgramData/Packages/!("MicrosoftWindows.Client.CBS_cw5n1h2txyewy")
+# rm -rf Users/*/AppData/Local/Microsoft/WindowsApps/!("MicrosoftWindows.Client.CBS_cw5n1h2txyewy")
+# rm -rf Users/*/AppData/Local/Packages/!("Microsoft.Windows.ShellExperienceHost_cw5n1h2txyewy"|"windows.immersivecontrolpanel_cw5n1h2txyewy"|"MicrosoftWindows.Client.CBS_cw5n1h2txyewy")
+# rm -rf Windows/SystemApps/!("ShellExperienceHost_cw5n1h2txyewy"|"Microsoft.UI.Xaml.CBS_8wekyb3d8bbwe"|"MicrosoftWindows.Client.CBS_cw5n1h2txyewy"|"MicrosoftWindows.Client.Core_cw5n1h2txyewy")
 rm -f "Windows/System32/smartscreen.exe"
-rm -f "Windows/System32/mobsync.exe"
+# rm -f "Windows/System32/mobsync.exe"
 
 echo info: done
 
